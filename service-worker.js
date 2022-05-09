33a7e25e282c174089e912b33f5f1d8c47e86825
Khushal Sethi <29970524+missionfission@users.noreply.github.com>
2022-05-09 09:08:16 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dalle-playground/precache-manifest.af2a6105a766b09a8bd1686884938a00.js"
+  "/godel-playground/precache-manifest.af2a6105a766b09a8bd1686884938a00.js"
 );
 
 self.addEventListener('message', (event) => {
